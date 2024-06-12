2740c1423d3bbd55ed6164c8ca100d1240b828f5
NAJEEHAT APEKE WAHAB <wahabnanjeehat@gmail.com>
2024-06-12 15:12:26 +0000

    Update script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+const container=document.querySelector(".container")
+const search=document.querySelector(".search-box button")
+const weatherBox=document.querySelector(".weather-box")
+const weatherDetails=document.querySelector(".weather-details")
+
+search.addEventListener('click',()=>{
+    const APIKey='5e8fd5f3cd8bb381651b4f81b7e9a867';
+    const city= document.querySelector('.search-box input').value;
+
+    if(city=='')
+        return
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}')
+    .then(response=>response.json)
+    .then(json=>{
+        const image = document.querySelector('.weather-box img')
+        const temperature = document.querySelector('.weather-box .temperature')
+        const description = document.querySelector('.weather-box .description')
+        const humidity = document.querySelector('.weather-details .humidity span')
+        const wind = document.querySelector('.weather-details .wind span')
+
+
+        switch(json.weather[0].main){
+            case'Clear':
+            image.scr = 'imag'
+        }
+    })
+
+
+
+
+})
